c264fb08297bb3197dbad853e87712a42e9d2f6c
Tara Gamache <tgamache.13@gmail.com>
2019-11-10 17:03:03 -0800

    rendering characters

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return(
+    <div>
+      <img src={props.character.image} alt="Photo of {props.character.name}" />
+      <h1>{props.character.name}</h1>
+      <h2>{props.character.gender}</h2>
+      <h2>{props.character.species}</h2>
+    </div>
+  );
 }
