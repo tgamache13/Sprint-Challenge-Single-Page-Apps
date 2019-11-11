031c4ac9ac126e51c5218d0bad994af0560d346c
Tara Gamache <tgamache.13@gmail.com>
2019-11-10 17:37:11 -0800

    hooked up welcome and charcter pages and functioning search

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Route } from "react-router-dom";
 
 import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([]);
+  const [found, setFound] = useState(false);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
@@ -19,13 +22,18 @@ export default function CharacterList() {
   }, [characters]);
 
   return (
-    <section className="character-list">
-      <h2>Character List</h2>
-      
-        {characters.map(character => (
-          <CharacterCard key={character.id} character={character}/>
-        ))}
-      
-    </section>
+    <div>
+      <SearchForm {...props} characters={characters} setFound={(ch) => setFound(ch)}/>
+      <h2>{found ? found.name : "Not Found"}</h2>
+      <Route path ='/character/:name' render={props => <CharacterCard {...props} character={found}/>}/>
+      <section className="character-list">
+        <h2>Character List</h2>
+        
+          {characters.map(character => (
+            <CharacterCard {...props} key={character.id} character={character}/>
+          ))}
+        
+      </section>
+    </div>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,46 @@
 import React, { useState } from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
-export default function SearchForm() {
+function SearchForm({ values, errors, touched }) {
  
   return (
     <section className="search-form">
-     // Add a search form here
+     <Form>
+       <div>
+         <Field 
+            type="text"
+            name="character"
+            placeholder="Character"
+          />
+         {touched.character && errors.character && <p>{errors.character}</p>}
+       </div>
+       <button>Submit</button>
+     </Form>
     </section>
   );
 }
+
+const FormikSearchForm = withFormik({
+  mapPropsToValues({ character }) {
+    return {
+      character: character || ""
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+    character: Yup.string()
+              .required("You must enter a character to search for."),
+  }),
+
+  handleSubmit(values, { props, resetForm, setErrors, setSubmitting }) {
+    for(let ch of props.characters){
+      if(ch.name.toLowerCase() == values.character.toLowerCase()){
+        props.setFound(ch);
+        props.history.push(`/character/${ch.name}`);
+      }
+    }
+  }
+})(SearchForm);
+
+export default FormikSearchForm;
